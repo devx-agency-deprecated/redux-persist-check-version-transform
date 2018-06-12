48049746e01224f5cb4f261049cf6208bf44a37d
Tomasz Pilch <tomasz@devx.agency>
2018-06-12 11:05:55 +0200

    missing build

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,70 @@
-'use strict';Object.defineProperty(exports,'__esModule',{value:!0});var _assign=require('babel-runtime/core-js/object/assign'),_assign2=_interopRequireDefault(_assign),_stringify=require('babel-runtime/core-js/json/stringify'),_stringify2=_interopRequireDefault(_stringify),_keys=require('babel-runtime/core-js/object/keys'),_keys2=_interopRequireDefault(_keys),_reduxPersist=require('redux-persist'),_cryptoJs=require('crypto-js'),_cryptoJs2=_interopRequireDefault(_cryptoJs);function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}exports.default=function(a,b,c){return(0,_reduxPersist.createTransform)(function(b,d){if(a[d].blacklist){var e={};(0,_keys2.default)(b).map(function(c){a[d].blacklist.includes(c)||(e[c]=b[c])}),b=e}else if(a[d].whitelist){var f={};a[d].whitelist.map(function(a){f[a]=b[a]}),b=f}return a[d].encrypt?(delete b.mergeDeep,{data:_cryptoJs2.default.AES.encrypt((0,_stringify2.default)(b),c).toString(),version:a[d].version}):(b.version||(b.version=a[d].version),b)},function(b,d){if(a[d].version&&(!b.version||b.version!==a[d].version))return a[d].redux.INITIAL_STATE.asMutable();if(a[d].encrypt&&b.data){var e=_cryptoJs2.default.AES.decrypt(b.data,c);return JSON.parse(e.toString(_cryptoJs2.default.enc.Utf8))}return(0,_assign2.default)(a[d].redux.INITIAL_STATE.asMutable(),b)},{whitelist:b})};
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _assign = require('babel-runtime/core-js/object/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
+
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
+var _keys = require('babel-runtime/core-js/object/keys');
+
+var _keys2 = _interopRequireDefault(_keys);
+
+var _reduxPersist = require('redux-persist');
+
+var _cryptoJs = require('crypto-js');
+
+var _cryptoJs2 = _interopRequireDefault(_cryptoJs);
+
+var _seamlessImmutable = require('seamless-immutable');
+
+var _seamlessImmutable2 = _interopRequireDefault(_seamlessImmutable);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (reduxStore, persistWhitelist, r) {
+  return (0, _reduxPersist.createTransform)(function (inboundState, key) {
+    if (reduxStore[key].blacklist) {
+      var inboundStateTemp = {};
+      (0, _keys2.default)(inboundState).map(function (param) {
+        if (!reduxStore[key].blacklist.includes(param)) {
+          inboundStateTemp[param] = inboundState[param];
+        }
+      });
+      inboundState = inboundStateTemp;
+    } else if (reduxStore[key].whitelist) {
+      var _inboundStateTemp = {};
+      reduxStore[key].whitelist.map(function (param) {
+        _inboundStateTemp[param] = inboundState[param];
+      });
+      inboundState = _inboundStateTemp;
+    }
+
+    if (reduxStore[key].encrypt) {
+      delete inboundState['mergeDeep'];
+      return {
+        data: _cryptoJs2.default.AES.encrypt((0, _stringify2.default)(inboundState), r).toString(),
+        version: reduxStore[key].version
+      };
+    } else {
+      if (!inboundState.version) {
+        inboundState.version = reduxStore[key].version;
+      }
+      return inboundState;
+    }
+  }, function (outboundState, key) {
+    if (reduxStore[key].version && (!outboundState.version || outboundState.version !== reduxStore[key].version)) {
+      return reduxStore[key].redux.INITIAL_STATE;
+    } else if (reduxStore[key].encrypt && outboundState.data) {
+      var bytes = _cryptoJs2.default.AES.decrypt(outboundState.data, r);
+      return (0, _seamlessImmutable2.default)(JSON.parse(bytes.toString(_cryptoJs2.default.enc.Utf8)));
+    }
+    return (0, _assign2.default)(reduxStore[key].redux.INITIAL_STATE, outboundState);
+  }, { whitelist: persistWhitelist });
+};
